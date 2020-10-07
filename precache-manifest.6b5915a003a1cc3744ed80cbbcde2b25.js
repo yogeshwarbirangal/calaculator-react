2039cb81bd1d7f7b63488f214cb618e6c53e7046
Yogeshwar <yogeshwarbirangal@gmail.com>
2020-10-07 21:30:50 +0530

    Updates

---
+++ precache-manifest.6b5915a003a1cc3744ed80cbbcde2b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28de9eb9331fd6926d69234c864debd",
+    "url": "/calaculator-react/index.html"
+  },
+  {
+    "revision": "5ce5d31246218cc33033",
+    "url": "/calaculator-react/static/css/main.60df8b5f.chunk.css"
+  },
+  {
+    "revision": "c1c84dac2c53ceee3f29",
+    "url": "/calaculator-react/static/js/2.b55aaf20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calaculator-react/static/js/2.b55aaf20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce5d31246218cc33033",
+    "url": "/calaculator-react/static/js/main.b9630416.chunk.js"
+  },
+  {
+    "revision": "4ee0ba1e7c27b31b6ac8",
+    "url": "/calaculator-react/static/js/runtime-main.75da2b92.js"
+  }
+]);
